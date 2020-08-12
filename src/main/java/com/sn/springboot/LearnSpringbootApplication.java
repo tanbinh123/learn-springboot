83c56a2b5c4004ccbc16f3f8035f17653b5b5787
shehuan <shehuan@nowledgedata.com.cn>
2020-08-12 11:37:15 +0800

    fix

--- src/main/java/com/sn/springboot/LearnSpringbootApplication.java
+++ src/main/java/com/sn/springboot/LearnSpringbootApplication.java
@@ -1,7 +1,7 @@
 package com.sn.springboot;
 
 import com.sn.springboot.aop.GreetAspect;
-import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 
 @SpringBootApplication
 @PropertySource(value = {"classpath:jdbc.properties"}, ignoreResourceNotFound = true)
-//@MapperScan(basePackages = "com.sn.springboot.dao", annotationClass = Repository.class)
+@MapperScan(basePackages = "com.sn.springboot.dao", annotationClass = Repository.class)
 public class LearnSpringbootApplication {
 
     @Bean
@@ -18,14 +18,14 @@ public class LearnSpringbootApplication {
         return new GreetAspect();
     }
 
-    @Bean
-    public MapperScannerConfigurer mapperScannerConfigurer() {
-        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
-        mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactory");
-        mapperScannerConfigurer.setBasePackage("com.sn.springboot.dao");
-        mapperScannerConfigurer.setAnnotationClass(Repository.class);
-        return mapperScannerConfigurer;
-    }
+//    @Bean
+//    public MapperScannerConfigurer mapperScannerConfigurer() {
+//        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+//        mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactory");
+//        mapperScannerConfigurer.setBasePackage("com.sn.springboot.dao");
+//        mapperScannerConfigurer.setAnnotationClass(Repository.class);
+//        return mapperScannerConfigurer;
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(LearnSpringbootApplication.class, args);
