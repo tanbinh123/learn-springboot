80a0b13bd83e80110e638d64f508e3e62a715468
SheHuan <she320955huan>
2020-09-12 22:55:26 +0800

    文件上传

--- src/main/java/com/sn/springboot/controller/UploadController.java
+++ src/main/java/com/sn/springboot/controller/UploadController.java
@@ -8,7 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -33,5 +35,34 @@ public class UploadController {
             e.printStackTrace();
         }
         return "error";
+    }
+
+    /**
+     * 多文件上传，如果是单个input则用数组接收，否则用多个MultipartFile
+     * @param files
+     * @param req
+     * @return
+     */
+    @PostMapping("/uploads")
+    public List<String> uploads(MultipartFile[] files, HttpServletRequest req) {
+        String format = sdf.format(new Date());
+        String realPath = req.getServletContext().getRealPath("/img/") + format;
+        File dest = new File(realPath);
+        if (!dest.exists()) {
+            dest.mkdirs();
+        }
+        List<String> urls = new ArrayList<>();
+        for (MultipartFile file : files) {
+            String name = file.getOriginalFilename();
+            String newName = UUID.randomUUID().toString().replaceAll("-", "") + name.substring(name.lastIndexOf("."));
+            try {
+                file.transferTo(new File(dest, newName));
+                String url = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() + req.getContextPath() + "/img/" + format + "/" + newName;
+                urls.add(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return urls;
     }
 }
