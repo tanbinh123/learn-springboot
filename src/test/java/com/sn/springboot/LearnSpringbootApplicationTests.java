075677580de179b582903d47229e247047ec6e78
SheHuan <she320955huan>
2020-09-20 14:19:34 +0800

    fix

--- src/test/java/com/sn/springboot/LearnSpringbootApplicationTests.java
+++ src/test/java/com/sn/springboot/LearnSpringbootApplicationTests.java
@@ -3,9 +3,12 @@ package com.sn.springboot;
 import com.sn.springboot.aop.CoffeeShop;
 import com.sn.springboot.properties.DataBaseProperties3;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class LearnSpringbootApplicationTests {
 
